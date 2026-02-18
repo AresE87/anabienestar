ddcd164e5cc4c5f739a9510933dff43ffbc1d70e
Edgardo Agustin Trujillo Mendieta <eatrujil@ccu.uy>
2026-02-18 17:07:55 -0300

    fix: deshabilitar LockManager en supabase client

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -3,4 +3,11 @@ import { createClient } from '@supabase/supabase-js'
 const supabaseUrl = 'https://rnbyxwcrtulxctplerqs.supabase.co'
 const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJuYnl4d2NydHVseGN0cGxlcnFzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE0MjQ3MjIsImV4cCI6MjA4NzAwMDcyMn0.gwWyvyqk8431wvejeswrnxND1g_EpMRNVx8JllU7o-g'
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+  auth: {
+    persistSession: true,
+    storageKey: 'anabienestar-auth',
+    storage: window.localStorage,
+    lock: false,
+  },
+})
