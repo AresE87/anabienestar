14268a6674cf68f7f81527ff700d792524ab3d6f
Edgardo Agustin Trujillo Mendieta <eatrujil@ccu.uy>
2026-02-18 16:02:12 -0300

    feat: material y videos con Supabase, ocultar nav en admin

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import { AppProvider } from './context/AppContext';
 import Home from './screens/Home';
 import Progreso from './screens/Progreso';
@@ -9,19 +9,30 @@ import Citas from './screens/Citas';
 import Admin from './screens/Admin';
 import BottomNav from './components/BottomNav';
 
+function AppContent() {
+  const location = useLocation();
+  const isAdmin = location.pathname.startsWith('/admin');
+
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/progreso" element={<Progreso />} />
+        <Route path="/recetas" element={<Recetas />} />
+        <Route path="/material" element={<Material />} />
+        <Route path="/citas" element={<Citas />} />
+        <Route path="/admin" element={<Admin />} />
+      </Routes>
+      {!isAdmin && <BottomNav />}
+    </>
+  );
+}
+
 function App() {
   return (
     <Router>
       <AppProvider>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/progreso" element={<Progreso />} />
-          <Route path="/recetas" element={<Recetas />} />
-          <Route path="/material" element={<Material />} />
-          <Route path="/citas" element={<Citas />} />
-          <Route path="/admin" element={<Admin />} />
-        </Routes>
-        <BottomNav />
+        <AppContent />
       </AppProvider>
     </Router>
   );
